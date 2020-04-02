3fcaf38cd68f6a978c26c651cd67fdca1e0b4af1
George Carder <george@archon.cloud>
2020-04-02 10:31:22 -0700

    removed iaccount from dht config

--- archon/config.go
+++ archon/config.go
@@ -2,15 +2,13 @@ package archon_dht
 
 import (
 	permLayer "github.com/archoncloud/archon-dht/permission_layer"
-	"github.com/archoncloud/archoncloud-go/interfaces"
 	"github.com/pariz/gountries"
 )
 
 type DHTConnectionConfig struct {
-	Seed                  int64 // seed to initialize fresh dht rsa keyset and id
-	Global                bool  // bootstrap to global set
-	IAmBootstrap          bool  // declare if self is a bootstrap node
-	Account               interfaces.IAccount
+	Seed                  int64           // seed to initialize fresh dht rsa keyset and id
+	Global                bool            // bootstrap to global set
+	IAmBootstrap          bool            // declare if self is a bootstrap node
 	OptInToNetworkLogging bool            // self-explanatory
 	CountryCode           gountries.Codes // self-reported country code
 	PermissionLayer       permLayer.PermissionLayer
